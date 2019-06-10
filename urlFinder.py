b20a490c424cb603eb6f3c96750925e15f94827c
DracoBengali86 <AlexanderCHummel@gmail.com>
2019-06-10 17:19:19 -0500

    refactor part of search and make search more universal

--- urlFinder.py
+++ urlFinder.py
@@ -4,9 +4,17 @@
 
 import requests
 import os
-#import re
+import re
 import bs4  # beautifulSoup4
 from htmlCreator import buildComicPage
+
+nextLinkParent = False
+nextTag = []
+nextAttr = []
+nextStr = []
+nextTag.append("a")
+nextAttr.append("rel")
+nextStr.append("next")
 
 
 def urlBuild(urlFirstPage, filename, urlMain=None):
@@ -28,7 +36,12 @@ def urlBuild(urlFirstPage, filename, urlMain=None):
                 writeURL = False
                 url = lines[pagecount - 1]
 
-    while not url.endswith('#'):  # on latest page url under 'Next' button ends with '#'
+    if len(nextTag) != len(nextAttr) or len(nextTag) != len(nextStr):
+        print("Search conditions length mismatch")
+        return pagecount
+    searchLength = len(nextTag)
+
+    while not url.endswith('#') and not url.endswith('zzzbreak'):  # on latest page url under 'Next' button ends with '#'
         # Download page
         print('Finding page %s...' %url)
         res = requests.get(url)
@@ -50,8 +63,28 @@ def urlBuild(urlFirstPage, filename, urlMain=None):
             writeURL = True
 
         # Get the Next button's URL
-        nextLink = soup.select('a[rel="next"]')[0]
-        url = 'http://xkcd.com' + nextLink.get('href')
+        nextTemp = None
+        j = 0
+        while nextTemp is None:
+            if j < searchLength:
+                #nextTemp = soup.find(nextTag1, rel="next")
+                nextTemp = soup.find(nextTag[j], {nextAttr[j]: re.compile(nextStr[j])})
+            else:
+                print("next link couldn't be found")
+                print("last found page: " + url)
+                break
+            j += 1
+
+        if nextTemp is None:
+            nextPage = 'zzzbreak'
+        else:
+            if nextLinkParent:
+                nextLink = nextTemp.parent
+            else:
+                nextLink = nextTemp
+            nextPage = nextLink.get('href')
+
+        url = 'http://xkcd.com' + nextPage
 
     pagecount = pagecount + i
     print('Done. Current Pagecount: ' + str(pagecount))
